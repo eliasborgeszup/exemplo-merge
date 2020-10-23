641c4bf834a039e4c6d3de3abab15d170dc08447
GrazielaRO <grazi375@hotmail.com>
2020-10-23 14:57:41 -0300

    metodo inserir

--- src/main/java/br/com/exemplo/merge/controller/AlunoController.java
+++ src/main/java/br/com/exemplo/merge/controller/AlunoController.java
@@ -1,5 +1,26 @@
 package br.com.exemplo.merge.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import br.com.exemplo.merge.entity.AlunoEntity;
+import br.com.exemplo.merge.repository.AlunoRepository;
+
+@Controller
 public class AlunoController {
+	
+	@Autowired
+	AlunoRepository repository;
+
+	@PostMapping
+	public AlunoEntity insereAluno(@RequestBody AlunoEntity aluno) {
+		if (repository.existsById(aluno.getMatricula())) {
+			return null;
+		}
+		
+		return this.repository.save(aluno);
+	}
 
 }
--- src/main/java/br/com/exemplo/merge/entity/AlunoEntity.java
+++ src/main/java/br/com/exemplo/merge/entity/AlunoEntity.java
@@ -1,5 +1,63 @@
 package br.com.exemplo.merge.entity;
 
-public class AlunoEntity {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class AlunoEntity {
+	
+	@Id
+	private Long matricula;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(name = "data_nascimento", nullable = false)
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private LocalDate dataNascimento;
+	
+	@Column(unique = true)
+	private String cpf;
+	
+	@Column(name = "valor_mensalidade", nullable = false)
+	private Float valorMensalidade;
+	
+	
+	public Long getMatricula() {
+		return matricula;
+	}
+	
+	public void setMatricula(Long matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Float getValorMensalidade() {
+		return valorMensalidade;
+	}
+	public void setValorMensalidade(Float valorMensalidade) {
+		this.valorMensalidade = valorMensalidade;
+	}
+	
 }
