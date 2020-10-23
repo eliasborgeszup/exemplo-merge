c73e60365440bb40be814a2cb88208ba04beb2ac
unknown <elias.borges@zup.com.br>
2020-10-23 14:57:36 -0300

    Função de deletar aluno

--- src/main/java/br/com/exemplo/merge/controller/AlunoController.java
+++ src/main/java/br/com/exemplo/merge/controller/AlunoController.java
@@ -1,5 +1,25 @@
 package br.com.exemplo.merge.controller;
 
-public class AlunoController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.exemplo.merge.repository.AlunoRepository;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+	@Autowired
+	AlunoRepository repository;
+	
+	@DeleteMapping(path = "/{matricula}")
+	public boolean removeAluno(@PathVariable Long matricula) {	
+		if(repository.existsById(matricula)) {
+			this.repository.deleteById(matricula);
+			return true;
+		}
+		return false;
+	}
 }
--- src/main/java/br/com/exemplo/merge/repository/AlunoRepository.java
+++ src/main/java/br/com/exemplo/merge/repository/AlunoRepository.java
@@ -1,5 +1,10 @@
 package br.com.exemplo.merge.repository;
 
-public class AlunoRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-}
+import br.com.exemplo.merge.entity.AlunoEntity;
+
+
+@Repository
+public interface AlunoRepository extends CrudRepository<AlunoEntity, Long> {}
