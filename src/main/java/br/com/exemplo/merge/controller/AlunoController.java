d813d596f54db670cdead11bd1ff3ae83dfe8f6d
gabrielgarciazup <gabriel.garcia@zup.com.br>
2020-10-23 14:58:32 -0300

    [UPDATE] alteraAluno

--- src/main/java/br/com/exemplo/merge/controller/AlunoController.java
+++ src/main/java/br/com/exemplo/merge/controller/AlunoController.java
@@ -1,5 +1,26 @@
 package br.com.exemplo.merge.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.exemplo.merge.entity.Aluno;
+import br.com.exemplo.merge.repository.AlunoRepository;
+
+@RestController
+@RequestMapping("/alunos")
 public class AlunoController {
+
+	@Autowired
+	AlunoRepository repository;
+		
+	@PutMapping(path = "/{matricula}", produces = {MediaType.APPLICATION_JSON_VALUE})
+	public Aluno alteraAluno(@PathVariable Long matricula, @RequestBody Aluno aluno) {
+		return this.repository.save(aluno);
+	}
 
 }
